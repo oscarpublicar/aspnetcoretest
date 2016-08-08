93f0575d65f357c81e9a5ce563ce3099af43eb0d
Oscar Buitrago <oscar_andre_3@hotmail.com>
2016-08-08 15:24:55 -0500

    change install sh

--- shell/install.sh
+++ shell/install.sh
@@ -1,3 +1,4 @@
+/usr/bin/supervisorctl stop app
 /usr/local/bin/dotnet restore /tmp/aspnetcore/src/AspNETCoreTest/project.json
 /usr/local/bin/dotnet publish /tmp/aspnetcore/src/AspNETCoreTest/project.json -c release -o /release
  
