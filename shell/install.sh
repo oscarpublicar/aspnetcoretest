f2eff97276a248c4d1feef308da113bcf7a13a4c
oscarpublicar <oscar.buitrago@publicar.com>
2016-08-08 11:29:56 -0500

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -1,4 +1,4 @@
 /usr/local/bin/dotnet restore /tmp/aspnetcore/src/AspNETCoreTest/project.json
 /usr/local/bin/dotnet publish /tmp/aspnetcore/src/AspNETCoreTest/project.json -c release -o /release
  
-/usr/local/bin/dotnet /release/AspNETCoreTest.dll&
+# /usr/local/bin/dotnet /release/AspNETCoreTest.dll
