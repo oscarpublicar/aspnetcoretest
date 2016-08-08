1d22ab906e1f2217ffabf6274a0a349122549111
Oscar Buitrago <oscar_andre_3@hotmail.com>
2016-08-08 17:55:40 -0500

    ASCASDFASF

--- shell/install.sh
+++ shell/install.sh
@@ -1,5 +1,5 @@
 /usr/bin/supervisorctl stop app
 /usr/local/bin/dotnet restore /tmp/aspnetcore/src/AspNETCoreTest/project.json
 /usr/local/bin/dotnet publish /tmp/aspnetcore/src/AspNETCoreTest/project.json -c release -o /release
- 
+rm -rf /tmp/aspnetcore/
 # /usr/local/bin/dotnet /release/AspNETCoreTest.dll
